62a67b39990801a0ba6caee51f579e0ff1f71c55
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-07-11 08:23:49 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+undefined
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right= parseInt(rightNumbers, 10);
+
+  if (right > 40 && right < 360) {
+    dodger.style.left = `${right + 1}px`;
+  }else{
+    dodger,style.left = `${right - 1}px`
+  }
+}
+document.addEventListener('keydown', (e)=>{
+  if (e.key === "ArrowRight"){
+    moveDodgerRight()
+  }
+})
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+  
